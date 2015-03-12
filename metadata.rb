f1deccdedf046ac707bd1e0a00a54d8daf59c490
Jeremy Olliver <jeremy.olliver@gmail.com>
2015-03-12 16:29:52 +1300

    Set release version to match gitlab

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,8 @@ maintainer_email 'jeremy.olliver@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures gitlab-omnibus'
 long_description 'Installs/Configures gitlab-omnibus'
-version          '0.1.0'
+version          '7.8.0'
+
+['ubuntu', 'debian', 'redhat', 'centos', 'amazon', 'scientific'].each do |os|
+  supports os
+end
